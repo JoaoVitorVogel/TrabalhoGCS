cfa60365a62e54aa8043a5baaf17e426729442cb
Guilherme <guilhermebcavali@gmail.com>
2024-06-07 13:59:53 -0300

    alterei umas linhas de Usuário

--- controlePedidos/src/models/usuarios/Usuario.java
+++ controlePedidos/src/models/usuarios/Usuario.java
@@ -4,16 +4,21 @@ import java.util.ArrayList;
 
 public class Usuario {
     private ArrayList<Usuario> usuarios_gerados;
-
     private String nome;
+    private int id;
+
+    public Usuario(String nome) {
+        setId();
+        this.nome = nome;
+    }
+
     public String getNome() {
         return this.nome;
     }
     public void setNome(String newNome) {
         this.nome = newNome;
     }
-
-    private int id;
+    
     private void setId() {
         if (usuarios_gerados == null) usuarios_gerados = new ArrayList<>();
         int newId = usuarios_gerados.size();
@@ -22,11 +27,6 @@ public class Usuario {
     }
     public int getId() {
         return this.id;
-    }
-
-    public Usuario(String nome) {
-        setId();
-        this.nome = nome;
     }
 
 }
