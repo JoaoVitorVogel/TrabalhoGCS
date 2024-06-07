60bee1a6878ff355f34a03b2d91d4cab398f1734
Guilherme <guilhermebcavali@gmail.com>
2024-06-07 16:10:30 -0300

    pequena alteracao

--- controlePedidos/src/App.java
+++ controlePedidos/src/App.java
@@ -2,11 +2,12 @@ import menus.Menu;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Menu teste = new Menu();
-        teste.clearConsole();
-        teste.showLogo();
-        teste.showOptions();
-        teste.criarUsuarios();
-        teste.showTodosFuncionarios();
+        Menu menu = new Menu();
+        menu.clearConsole();
+        menu.showLogo();
+        menu.showOptions();
+        menu.criarUsuarios();
+        menu.criarDepartamentos();
+        menu.showTodosFuncionarios();
     }
 }
