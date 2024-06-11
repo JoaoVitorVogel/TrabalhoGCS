0871a880974a80e5e60aab97376555da0a7ef95f
gabriellrmartins <gabriellrmartins@gmail.com>
2024-06-11 15:58:20 -0300

    importing TipoUsuario into Funcionario

--- controlePedidos/src/models/usuarios/funcionarios/Funcionario.java
+++ controlePedidos/src/models/usuarios/funcionarios/Funcionario.java
@@ -3,6 +3,7 @@ package models.usuarios.funcionarios;
 // import java.util.ArrayList;
 
 import models.usuarios.Usuario;
+import models.usuarios.TipoUsuario;
 
 public class Funcionario extends Usuario {
     public Funcionario(String nome, String departamento) {
